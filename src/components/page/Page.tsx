d6f7a6f6bb8119185de5328a9e70903aba407d4a
AudunSorheim <audun.sorheim@nav.no>
2023-11-09 18:30:00 +0100

    Fix build error

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react"
-import { MedUtenAGVisning } from "../../pages/aktivitetsplikt"
 import { PageHeadingWithImage } from "./PageHeadingWithImage"
 import { PageHeading } from "./PageHeading"
+import { MedUtenAGVisning } from "@/components/AktivitetskravInfoComponent";
 
 interface Props {
     headerText: string
