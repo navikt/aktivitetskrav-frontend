772c229b10b18cd336e5f4872d311634ae317733
AudunSorheim <audun.sorheim@nav.no>
2023-11-09 14:52:55 +0100

    Use esyfo-proxy

--- src/api/fetchAktivitetskrav.ts
+++ src/api/fetchAktivitetskrav.ts
@@ -3,11 +3,14 @@ import { AktivitetskravVurdering } from "@/schema/aktivitetskravVurderingSchema"
 import fixtures from "@/mocks/fixtures";
 
 export async function fetchAktivitetskrav() {
-  if (process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT === "local") {
+  if (
+    process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT === "local" ||
+    process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT === "demo"
+  ) {
     return fixtures.unntakVurdering;
   }
 
-  const res = await fetch("/api/aktivitetskrav");
+  const res = await fetch(process.env.ESYFO_PROXY_API_URL!);
   const { data }: { data: AktivitetskravVurdering } = await res.json();
   return data;
 }
