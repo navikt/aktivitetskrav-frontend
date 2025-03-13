b176dc2472ba73e89f777b7e75e8b70f1bf25290
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2025-03-13 17:46:17 +0100

    WIP egen komponent

--- src/components/view/ForhandsvarselComponent.tsx
+++ src/components/view/ForhandsvarselComponent.tsx
@@ -5,6 +5,7 @@ import { AktivitetskravVurdering } from "@/schema/aktivitetskravVurderingSchema"
 import { getShortDateFormat } from "@/utils/dateUtils";
 import { ComponentHeader } from "@/components/header/ComponentHeader";
 import { useFerdigstillForhandsVarsel } from "@/data/ferdigstillVarsel";
+import { AlertForUsersAffectedByNoNotificationsBug } from "./alertForUsersAffectedByNoNotificationsBug/AlertForUsersAffectedByNoNotifications";
 
 interface Props {
   vurdering: AktivitetskravVurdering;
@@ -15,27 +16,11 @@ export const ForhandsvarselComponent = ({ vurdering }: Props) => {
 
   if (vurdering.status !== "FORHANDSVARSEL") return null;
 
-  const showInformationForUsersThatDidNotGetVarselBetweenFeb27AndMarSomething =
-    true;
-  // (new Date(vurdering.createdAt) >= new Date("2025-02-27") &&
-  //   new Date(vurdering.createdAt) <= new Date("2025-03-?") &&
-  //   new Date(vurdering.fristDato) <= new Date("2021-03-30")); // ?
-
-  const informationForUsersThatDidNotGetVarselBetweenFeb27AndMarSomething = (
-    <BodyLong>
-      På grunn av en teknisk feil har du ikke fått varsel på SMS og på Din side
-      om denne vurderingen til riktig tidspunkt. Du har derfor senere frist enn
-      det som står... (TODO) Vi beklager feilen.
-    </BodyLong>
-  );
-
   return (
     <div>
-      {showInformationForUsersThatDidNotGetVarselBetweenFeb27AndMarSomething && (
-        <Alert variant="info">
-          {informationForUsersThatDidNotGetVarselBetweenFeb27AndMarSomething}
-        </Alert>
-      )}
+      <AlertForUsersAffectedByNoNotificationsBug
+        forhandsvarselVurdering={vurdering}
+      />
 
       <div className="flex flex-col gap-4 mb-4">
         {vurdering.document?.map((d, index) => {
