1bdea0f4587a85df9ebfb8c60ff522efd28c08fd
André Wittwer <andre.wittwer@nav.no>
2026-01-14 13:57:40 +0100

    feat: add csp header (#1214)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,45 @@
 /** @type {import('next').NextConfig} */
+
+const environment =
+  process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT === "prod" ? "prod" : "dev";
+
+const SELF = "'self'";
+
+const appDirectives = {
+  "default-src": [SELF],
+  "script-src": [SELF, "'unsafe-eval'"],
+  "script-src-elem": [SELF],
+  "style-src": [SELF],
+  "style-src-elem": [SELF],
+  "img-src": [SELF, "data:"],
+  "font-src": [SELF, "https://cdn.nav.no"],
+  "worker-src": [SELF],
+  "connect-src": [SELF, "https://*.nav.no"],
+};
+
 const nextConfig = {
+  async headers() {
+    const { buildCspHeader } = await import(
+      "@navikt/nav-dekoratoren-moduler/ssr/index.js"
+    );
+    const cspValue = await buildCspHeader(appDirectives, { env: environment });
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspValue,
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
-  output: 'standalone',
-  basePath: '/syk/aktivitetskrav',
+  output: "standalone",
+  basePath: "/syk/aktivitetskrav",
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || "",
   productionBrowserSourceMaps: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
