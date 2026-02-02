08bb46846b800b76d5da01b5f7832b35b7e05e13
André Wittwer <andre.wittwer@nav.no>
2026-02-02 16:34:16 +0100

    chore: fix design

--- src/components/box/AktivitetskravBox.tsx
+++ src/components/box/AktivitetskravBox.tsx
@@ -9,11 +9,9 @@ export const AktivitetskravBox = ({ children }: Props) => {
   return (
     <div className="max-w-3xl w-full">
       <Box
-        background="bg-default"
-        padding="6"
-        borderRadius="medium"
-        shadow="small"
-        className="mx-4 mt-4 flex flex-col gap-4"
+        background="default"
+        padding="space-24"
+        className="mx-4 mt-4 flex flex-col gap-4 rounded-lg"
       >
         {children}
       </Box>
