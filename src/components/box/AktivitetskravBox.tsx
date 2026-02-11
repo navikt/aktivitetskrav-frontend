d11aeeed3da31e5bdd2fe3dc2181d5e9e957558e
André Wittwer <andre.wittwer@nav.no>
2026-02-11 11:45:17 +0100

    chore: dialog

--- src/components/box/AktivitetskravBox.tsx
+++ src/components/box/AktivitetskravBox.tsx
@@ -9,6 +9,7 @@ export const AktivitetskravBox = ({ children }: Props) => {
   return (
     <div className="max-w-3xl w-full">
       <Box
+        shadow="dialog"
         background="default"
         padding="space-24"
         className="mx-4 mt-4 flex flex-col gap-4 rounded-lg"
